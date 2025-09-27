4b5214049b761a48cd52cfabf2ae33cd908d4342
Ian Anderson <ian.kirk.anderson@gmail.com>
2025-09-27 17:35:34 +0100

    .

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -13,7 +13,13 @@ const Tab2: React.FC = () => {
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+
+
+
+
+
+
+            <IonTitle size="large">Photo Gallery</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 2 page" />
